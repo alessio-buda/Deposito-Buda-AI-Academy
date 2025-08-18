bc4a9443251fd5418df8b8426c95e2ed6247b8a2
Alessio Buda <alessio.buda@mail.polimi.it>
2025-08-18 11:54:25 +0200

    feat(main): add function stubs for file operations

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+def open_file(file_path):
+    pass
+
+def count_words(file_path):
+    pass
+
+def count_lines(file_path):
+    pass
+
+def  word_frequency(file_path):
+    pass
+
 def main():
     pass
